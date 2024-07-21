a577eff3dd541d0666f22039854ceaecfeadf28e
magic-kch <158145026+magic-kch@users.noreply.github.com>
2024-07-21 14:09:24 +0300

    Delete README.md.py

--- README.md.py
+++
@@ -1,9 +0,0 @@
-# Домашнее задание к лекции 2. «Iterators. Generators. Yield»
-
-1. Задание 1 `flat_iterator.py`
-
-2. Задание 2 `flat_generator.py`
-
-3. Задание 3 `new_iterator.py`
-
-4. Задание 4 `new_generator.py`
